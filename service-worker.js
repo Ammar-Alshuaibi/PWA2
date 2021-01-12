459aea1d35129630b3abc8b40d5b5a88c13c64cd
Ammar Alshuaibi <49941549+Ammar-Alshuaibi@users.noreply.github.com>
2021-01-12 17:47:05 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,24 @@
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        fetch(event.request).catch(function () {
-            return caches.match(event.request);
-        })
-    );
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      window.location.forceReload(true)
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
